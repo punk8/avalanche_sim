3888719bbc04b21cec6b44c91dbdd9aa87053ee4
陈诗鹏 <chenshipeng@chenshipengdeMacBook-Air.local>
2019-09-30 08:53:06 +0800

    modify

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## Avalanche共识模拟
+
+###Slush
+###Snow
+如果节点数量增加 相应的beta也要增加
--- src/main/java/com/punk/Constants/Constants.java
+++ src/main/java/com/punk/Constants/Constants.java
@@ -3,17 +3,17 @@ package com.punk.Constants;
 public final class Constants {
 
 
-    public static final double ALPHA =  0.25; //只要全网到达这个数就当作已经全网共识了
+    public static final double ALPHA =  0.2; //只要全网到达这个数就当作已经全网共识了
     public static final long DEFAULT_K = 10;
 
-    public static int N = 2000; //总共节点数
+    public static int N = 1000; //总共节点数
     public static int K = 31; // 抽样节点数
     public static int Alpha = 16; //同种颜色的个数超过alpha则设置颜色为该颜色
     public static int ROUND = 4;
-    public static int BETA = 5;
+    public static int BETA = 4;
 
 
-    public static int M = 200;//拜占庭节点个数
+    public static int M = (int) (N*ALPHA);//拜占庭节点个数
 
 
     public static final int BASEDLYBTWRP = 2000;				//节点之间的基础网络时延2s
